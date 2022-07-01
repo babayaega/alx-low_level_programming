219724c7ef8c8aa66f5086feedeb9327ede53bbd
Ibrahim Bakarr <ibakarr@gmail.com>
2022-07-01 19:43:35 +0000

    q2 1

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 /**
  * main - main entry point of the program
- * return: exit status of the program
+ * Return: exit status of the program
  */
 
-int main(void)
+int main (void)
 {
 	char letter;
+
 	for (letter = 'a'; letter <= 'z'; letter++)
 	{
 		putchar(letter);
