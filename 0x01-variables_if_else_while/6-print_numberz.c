960cc63782c46c9ac827d2e3c9213d4b9ebfd7b9
Ibrahim Bakarr <ibakarr@gmail.com>
2022-07-01 21:31:31 +0000

    q6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - main entry point of the program
+ * Return: exit code of the program
+*/
+
+int main(void)
+{
+	int num;
+
+	for (num = 0; num < 10; num++)
+		putchar(num);
+	putchar(num);
+	return (0);
+}
