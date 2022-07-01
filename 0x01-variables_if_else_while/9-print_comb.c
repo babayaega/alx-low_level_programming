a068b4c2b526412ab10710236e8b1a4572eafbf7
Ibrahim Bakarr <ibakarr@gmail.com>
2022-07-01 22:16:14 +0000

    rest of mandatory questions

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - main entry point of the program
+ *
+ * Return: exit status of the program
+ */
+int main(void)
+{
+	int num;
+
+	for (num = 0; num <= 9; num++)
+	{
+		putchar(num + '0');
+		if (num < 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+
+	return (0);
+}
