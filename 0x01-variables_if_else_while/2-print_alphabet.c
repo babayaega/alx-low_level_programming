90e54bb0cf6b35c715f182bb595a78293f99ec82
Ibrahim Bakarr <ibakarr@gmail.com>
2022-07-01 19:41:31 +0000

    q2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - main entry point of the program
+ * return: exit status of the program
+ */
+
+int main(void)
+{
+	char letter;
+	for (letter = 'a'; letter <= 'z'; letter++)
+	{
+		putchar(letter);
+		putchar('\n');
+	}
+
+
+return(0);
+}
