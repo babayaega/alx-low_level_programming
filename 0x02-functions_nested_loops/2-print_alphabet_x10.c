3155bb8ce6a5c26f8a2ab1ca76822e1c96ad04c5
Ibrahim Bakarr <ibakarr@gmail.com>
2022-07-05 18:48:34 +0000

    q2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_alphabet - a function that prints the letters of the alphabet
+ *
+ * Return: the exit status of the program.
+ */
+
+void print_alphabet(void)
+{
+	int i, j;
+
+	for (j = 0; j < 9; j++)
+	{
+		for (i = 'a'; i <= 'z'; i++)
+		{
+			_putchar(i);
+		}
+	}
+	_putchar('\n');
+}
