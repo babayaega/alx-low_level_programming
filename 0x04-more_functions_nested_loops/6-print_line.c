71f68edc7cf8455fbf634411144863067058e400
Ibrahim Bakarr <ibakarr@gmail.com>
2022-07-08 19:24:26 +0000

    q6

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_line - a functions that prints a straight line
+ * @n: how many '_'s to make up the line
+ */
+
+void print_line(int n)
+{
+	char c;
+
+	for (c = 0; c < n; c++)
+		_putchar("_");
+	_putchar('\n');
+}
