94c2897ef17a884a0e56bcb69ff6b02bcfb05177
Ibrahim Bakarr <ibakarr@gmail.com>
2022-07-08 19:17:22 +0000

    q5

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * more_numbers - prints every number from 0-14 multiplied by 10
+ */
+
+void more_number(void)
+{
+	int i;
+	char c;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (c = 0; c <= 14; c++)
+		{
+			if (c > 9)
+			_putchar((c / 10) + '0');
+			_putchar((c % 10) + '0');
+		}
+		_putchar('\n');
+	}
+}
