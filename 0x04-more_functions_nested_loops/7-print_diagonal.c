6e763639859b5fa367931b500080b939520a52dc
Ibrahim Bakarr <ibakarr@gmail.com>
2022-07-08 19:33:32 +0000

    q7

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * print_diagonal - function that prints a single diagonal line
+ * @n: integer function parameter
+ */
+
+void print_diagonal(int n)
+{
+	int i, j;
+
+	if (n <= 0)
+		_putchar('\n');
+	for (i = 0; i < n; i++)
+	{
+		for (j = 0; j <= 1; j++)
+		{
+			if (i == j)
+			{
+				_putchar('\\');
+				_putchar('n');
+			}
+			else
+			{
+				_putchar(' ');
+			}
+		}
+	}
+}
