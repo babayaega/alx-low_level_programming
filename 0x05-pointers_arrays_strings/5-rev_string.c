4e8b318758c4d3a85b73219a0e6d5d3fd0c8e816
Ibrahim Bakarr <ibakarr@gmail.com>
2022-07-12 12:32:30 +0000

    5

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -9,6 +9,7 @@ void rev_string(char *s)
 {
 	int i = 0;
 	unsigned long j = 0;
+	char *holdingPointer = s;
 	char holdingArray[sizeof(s) - 1];
 
 	for (i = sizeof(s) - 1; i <= 0; i--)
@@ -16,7 +17,12 @@ void rev_string(char *s)
 		for (j = 0; j < sizeof(holdingArray); j++)
 		{
 			holdingArray[j] = *(s + i);
-			_putchar(holdingArray[j]);
 		}
 	}
+
+	for (j = 0; j < sizeof(holdingArray); j++)
+	{
+		*(holdingPointer + j) = holdingArray[j];
+	}
+
 }
