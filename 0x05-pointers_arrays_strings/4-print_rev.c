124b5858aa6e406ce80b251ba869bff2562f10c7
Ibrahim Bakarr <ibakarr@gmail.com>
2022-07-12 10:09:52 +0000

    q4 renamed

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_rev - function that prints string in reverse followed by a new line
+ * @s: input string
+ */
+
+void print_rev(char *s)
+{
+	int i= 0;
+	int offset = 0;
+
+	while (*(s + offset) != '\0')
+		offset++;
+
+
+	for (i = offset; i > 0; i--)
+		_putchar(*(s + i));
+
+	_putchar('\n');
+}
