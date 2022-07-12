72551e87e738a05f58f87177a245dc1354da532c
Ibrahim Bakarr <ibakarr@gmail.com>
2022-07-12 07:58:44 +0000

    first commit

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - a function that resets the value of an integer to 98
+ * @n: function parameter of integer to reset
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,2 @@
+#alx-low_level_programming
+Exercises on pointers, arrays and strings
