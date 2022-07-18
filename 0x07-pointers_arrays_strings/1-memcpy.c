e4ddbcc7276a84d2d1a36c71042e3f2c18990ee7
Ibrahim Bakarr <ibakarr@gmail.com>
2022-07-18 22:46:28 +0000

    ssvsvs

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -13,10 +13,9 @@ char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	unsigned int i = 0;
 
-	while (i < n)
+	for (; i < n; i++)
 	{
 		*(dest + i) = *(src + i);
-		i++;
 	}
 
 	return (dest);
