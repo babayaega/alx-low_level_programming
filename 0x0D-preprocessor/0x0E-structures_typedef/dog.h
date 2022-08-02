832f2d956d87a8c21158fda71d2490b914bc01c5
Ibrahim Bakarr <ibakarr@gmail.com>
2022-08-02 19:36:26 +0000

    q0 revised

--- 0x0D-preprocessor/0x0E-structures_typedef/dog.h
+++ 0x0D-preprocessor/0x0E-structures_typedef/dog.h
@@ -9,7 +9,6 @@
  * @owner: the owner of the dog
  */
 
-typedef struct dog dog_t;
 
 struct dog
 {
@@ -17,6 +16,9 @@ struct dog
 	float age;
 	char *owner;
 };
+
+
+typedef struct dog dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
